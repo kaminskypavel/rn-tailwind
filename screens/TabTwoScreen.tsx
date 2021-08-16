e72efec56a8d4ee4112675ca10330b3043794c50
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2021-08-16 16:53:03 +0300

    feat: integrate initial expo-camera

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,15 +1,57 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Camera } from "expo-camera";
+import React, { useEffect, useState } from "react";
+import {
+  TouchableOpacity,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import tw from "./../lib/tailwind";
 
 export default function TabTwoScreen() {
+  const [hasPermission, setHasPermission] = useState<boolean>(false);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [showCamera, setShowCamera] = useState<boolean>(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Open Camera</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+    <View style={tw`w-full h-full flex items-center justify-center`}>
+      <Pressable onPress={() => setShowCamera(true)}>
+        <Text style={tw`bg-mygreen p-4 rounded-xl text-white font-bold`}>
+          Open Camera
+        </Text>
+      </Pressable>
+
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
@@ -17,16 +59,25 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  camera: {
+    flex: 1,
+    width: "100%",
+    height: "40%",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
   },
 });
