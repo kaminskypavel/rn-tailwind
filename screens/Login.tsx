32c68750ad8b443894362de7e5683f92a93b9dd0
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2021-08-19 02:11:06 +0300

    feat: integate storage and firebasedb

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,20 +1,26 @@
 import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
 import React, { useEffect, useState } from "react";
-import { Pressable, Text, View } from "react-native";
+import { Pressable, Text, View, Image } from "react-native";
 import tw from "../lib/tailwind";
+import database from "@react-native-firebase/database";
+import storage from "@react-native-firebase/storage";
 
 export default function Login() {
   // Set an initializing state whilst Firebase connects
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState<FirebaseAuthTypes.User>();
+  const [photo, setPhoto] = useState("");
 
   // Handle user state changes
-  function onAuthStateChanged(user: any) {
+  async function onAuthStateChanged(user: any) {
+    const reference = await storage().ref("homer.jpg");
+    const photo = await reference.getDownloadURL();
+    setPhoto(photo);
     setUser(user);
     if (initializing) setInitializing(false);
   }
 
-  const login = () => {
+  const login = async () => {
     auth()
       .signInWithEmailAndPassword("test@diabeetus.com", "123456")
       .then(() => {
@@ -27,11 +33,14 @@ export default function Login() {
 
         console.error(error);
       });
+
+    const snapshot = await database().ref("/users/pavel").once("value");
+    console.log("User data: ", snapshot.val());
   };
   const logout = async () => {
     try {
       await auth().signOut();
-      console.log("User signed out");
+      console.log("User signed out1");
     } catch (error) {
       if (error.code === "auth/operation-not-allowed") {
         console.log("Enable anonymous in your firebase console.");
@@ -48,7 +57,7 @@ export default function Login() {
 
   if (initializing) return null;
 
-  if (!user) {
+  if (!user?.email) {
     return (
       <View>
         <Pressable onPress={login}>
@@ -61,14 +70,22 @@ export default function Login() {
   }
 
   return (
-    <View>
+    <View style={tw`flex items-center justify-center`}>
       <Pressable onPress={logout}>
         <Text style={tw`bg-red-400 p-4 m-10 rounded-xl text-center`}>
           Logout
         </Text>
       </Pressable>
 
-      <Text style={tw` p-4 m-10 border-2 text-xl text-center`}>
+      <Image
+        style={tw`rounded-full w-24 h-24`}
+        source={{
+          uri: photo,
+        }}
+      />
+
+      <Text style={tw` p-4 m-10 border-2  text-center`}>
+        {photo}
         {JSON.stringify(user)}
       </Text>
     </View>
